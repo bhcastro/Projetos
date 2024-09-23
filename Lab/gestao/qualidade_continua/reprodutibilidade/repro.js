5ac3d01bdeceb452db8748a0e25296c9ece5625b
Bruno Castro <bruno.hen.castro@gmail.com>
2024-09-22 22:46:49 -0300

    Adição
    Adição de mais calculadoras e mais tentativas de configuração da repro.

--- Lab/gestao/qualidade_continua/reprodutibilidade/repro.js
+++ Lab/gestao/qualidade_continua/reprodutibilidade/repro.js
@@ -1,15 +1,36 @@
-    const hbManha1 = Number(document.getElementById('hb__manha__1').value);
-    const hbTarde1 = Number(document.getElementById('hb__tarde__1').value);
-    const hbNoite1 = document.getElementById('hb__noite__1');
-    const outputHbTarde1 = document.getElementById('output__hb__tarde__1');
+const hbManha1 = document.getElementById('hb__manha__1');
+const hbTarde1 = document.getElementById('hb__tarde__1');
+const hbNoite1 = document.getElementById('hb__noite__1');
+const htManha1 = document.getElementById('ht__manha__1');
+const htTarde1 = document.getElementById('ht__tarde__1');
+const outputHbTarde1 = document.getElementById('output__hb__tarde__1');
+const outputHtTarde1 = document.getElementById('output__ht__tarde__1');
+const botaoCalcular = document.getElementById('botao__calcular');
+
+function executarTarde(){
+    pegarValores(hbManha1, hbTarde1);
+    pegarValores(htManha1, htTarde1);
+}
+
+function pegarValores(manha, tarde){
+    const valorManha = parseFloat(manha.value.replace(',', '.'));
+    const valorTarde = parseFloat(tarde.value.replace(',', '.'));
+    if(!isNaN(valorManha) && !isNaN(valorTarde) && valorManha != 0 && valorTarde !=0){
+        calcular(valorManha, valorTarde);
+    }else{
+        alert('Digite um número válido!');
+    }
+}
 
 function calcular(manha, tarde) {
     let desvio = ((tarde - manha) / manha) * 100 ;
     desvio = desvio.toFixed(2);
-    console.log(`${desvio}%`);
     mostrarValor(outputHbTarde1, desvio);
+    mostrarValor(outputHtTarde1, desvio);
 }
 
 function mostrarValor(output, valor){
-    output.value = `${valor}%`;
+    output.innerText = `${valor}%`;
 }
+
+botaoCalcular.onclick = executarTarde;
