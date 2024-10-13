e746d17fb15c967a682ab17a6a81d2bd90a082e5
Bruno Castro <bruno.hen.castro@gmail.com>
2024-10-13 10:41:35 -0300

    Ajuste no script do desvio padrao
    Ajustes nos calculos da pagina de desvio padrao

--- Lab/calculadoras/calc_dp/js/calc_dp.js
+++ Lab/calculadoras/calc_dp/js/calc_dp.js
@@ -1,5 +1,5 @@
-const inputValores = document.getElementById('input__valores');
-const listaValores = document.getElementById('lista__valores');
+const inputValoresDp = document.getElementById('input__valores__dp');
+const listaValoresDp = document.getElementById('lista__valores__dp');
 const valoresMedia = [];
 const valoresIntervalo = [];
 const outputMedia = document.getElementById('output__media');
@@ -8,16 +8,17 @@ const outputDesvio2 = document.getElementById('output__desvio__2');
 const outputDesvio3 = document.getElementById('output__desvio__3');
 const outputDesvioAlto = document.getElementById('output__desvio__alto');
 const outputDesvioBaixo = document.getElementById('output__desvio__baixo');
+const divValores = document.getElementById('div__valores');
 
 function pegarValores() {
-    let valorInput = Number(inputValores.value);
+    let valorInput = Number(inputValoresDp.value);
     if(!isNaN(valorInput) && valorInput > 0){
         valoresMedia.push(valorInput);
         calcularMedia();
-        calcularIntervalos()
+        calcularIntervalos();
         listarValores();
-        inputValores.value = "";
-        inputValores.focus();
+        inputValoresDp.value = "";
+        inputValoresDp.focus();
     }else{
         alert(`Digite um número válido!`);
     }
@@ -25,13 +26,7 @@ function pegarValores() {
 
 function removerValor(index) {
     const excluirValor = confirm(`Remover ${valoresMedia[index]}?`);
-    if(valoresMedia.length === 1 || valoresIntervalo.length === 1) {
-        valoresMedia.splice(index, 1);
-        valoresIntervalo.splice(index, 1);
-        calcularIntervalos()
-        listarValores();
-        outputMedia.value = '';
-    }else if (excluirValor === true) {
+if (excluirValor === true) {
         valoresMedia.splice(index, 1);
         valoresIntervalo.splice(index, 1);
         calcularMedia();
@@ -43,14 +38,14 @@ function removerValor(index) {
 }
 
 function calcularMedia() {
-    let media = valoresMedia.reduce((acc, val) => acc + val) / valoresMedia.length;
+    let media = valoresMedia.reduce((acc, val) => acc + val, 0) / valoresMedia.length;
     media = media.toFixed(2);
     mostrarValores(outputMedia, media);
     }
 
 function calcularIntervalos() {
-    let intervalo = 0
-    let media = valoresMedia.reduce((acc, val) => acc + val) / valoresMedia.length;
+    let intervalo = 0;
+    let media = valoresMedia.reduce((acc, val) => acc + val, 0) / valoresMedia.length;
     for (let i = 0; i < valoresMedia.length; i++) {
         intervalo = (valoresMedia[i] - media) ** 2;
         valoresIntervalo.push(intervalo);
@@ -63,24 +58,26 @@ function calcularDesvio(){
     let desvio = totalDesvio / valoresIntervalo.length;
     let raizDesvio= Math.sqrt(desvio);
     calcularLimites(raizDesvio);
-    listarValores
+    listarValores();
 }
 
 function calcularLimites(valorDesvio){
-    let media = valoresMedia.reduce((acc, val) => acc + val) / valoresMedia.length;
+    let media = valoresMedia.reduce((acc, val) => acc + val, 0) / valoresMedia.length;
     let desvio1 = valorDesvio;
     let desvio2 = valorDesvio * 2;
     let desvio3 = valorDesvio * 3;
     let limiteAlto = media + desvio3;
     let limiteBaixo = media - desvio3;
-    if (limiteBaixo <= 0){
-        limiteBaixo = 0;
-    }
+    if(valoresMedia.length > 1){
+        divValores.classList.remove('ocultar');
     mostrarValores(outputDesvio1, desvio1.toFixed(2));
     mostrarValores(outputDesvio2, desvio2.toFixed(2));
     mostrarValores(outputDesvio3, desvio3.toFixed(2));
     mostrarValores(outputDesvioAlto, limiteAlto.toFixed(2));
     mostrarValores(outputDesvioBaixo, limiteBaixo.toFixed(2));
+    }else{
+        divValores.classList.add('ocultar');
+    }
 }
 
 function mostrarValores(saida, valor) {
@@ -94,7 +91,7 @@ function listarValores(){
             Valor ${index+1}= ${valoresMedia[index]}
             <img  id="botao__remover" src="https://icons.iconarchive.com/icons/custom-icon-design/mono-general-4/24/trash-icon.png" width="24" height="24" onclick="removerValor(${index})">
         </li>`});
-    listaValores.innerHTML = novoValor
-    inputValores.value = "";
-    inputValores.focus();
+    listaValoresDp.innerHTML = novoValor
+    inputValoresDp.value = "";
+    inputValoresDp.focus();
 }
